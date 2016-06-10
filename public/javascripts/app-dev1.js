c5b9740fe9ff40028be87e6d90af2cc3c636f885
Edinson <jean_clude_dortosms@hotmail.com>
2016-06-10 17:38:21 -0300

    upload

--- public/javascripts/app-dev1.js
+++ public/javascripts/app-dev1.js
@@ -48,7 +48,6 @@ $(document).ready(function(){
 
 
 
-	$('.display').DataTable();
 	
 
 });
--- public/javascripts/buttons.js
+++ public/javascripts/buttons.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
 	$(".linkSave").flat('flat-t-cuatro');
+	$(".propiedadesLink").flat('flat-t-dos');
 
 });
