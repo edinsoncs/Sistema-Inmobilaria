657db82f01d9649f526f4f704b3e3c4efad4273d
Edinson <jean_clude_dortosms@hotmail.com>
2016-08-19 12:50:05 -0300

    fixeando

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if(req.user) {
+  	res.redirect('/panel');
+  } else {
+  	res.render('index', { title: 'Express' });
+  }
+
 });
 
 module.exports = router;
